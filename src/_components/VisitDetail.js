ab5c4138a215f754bfda20e43029c080b42680f6
Brian Baker <bakerman59@gmail.com>
2020-04-01 16:47:20 -0700

    Fixed visit detail screen

--- src/_components/VisitDetail.js
+++ src/_components/VisitDetail.js
@@ -1,38 +1,21 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
-import gql from "graphql-tag";
+import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router-dom";
 import { ShowQuestionaire, Loading, PatientInfo, PatientMessages } from "./";
 import { GET_PRESCRIPTION } from "../Graphql";
 
-const GET_VISIT = gql`
-  query getVisit($id: String!) {
-    visit(id: $id) {
-      id
-      status
-      type
-      createdAt
-      questionnaire
-      user {
-        firstName
-        lastName
-        birthDate
-        photoId
-        addresses {
-          active
-          addressOne
-          city
-          state
-          telephone
-          zipcode
-        }
-      }
-    }
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexDirection: "row"
   }
-`;
+}));
 
 export const VisitDetail = () => {
   const { id } = useParams();
+  const classes = useStyles();
+
   const { data, loading, error } = useQuery(GET_PRESCRIPTION, {
     variables: { id: id }
   });
@@ -42,10 +25,10 @@ export const VisitDetail = () => {
   if (!data) return <p>Not Found!</p>;
 
   return (
-    <>
+    <div className={classes.container}>
       <PatientInfo prescription={data.prescription} />
       <ShowQuestionaire questionnaire={data.prescription.visit.questionnaire} />
       <PatientMessages prescription={data.prescription} />
-    </>
+    </div>
   );
 };
