fe3b7e17ad07973ffebede3c292006548bf0baed
Brian Baker <bakerman59@gmail.com>
2020-09-02 01:58:45 -0700

    Update server selection

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const dev_endpoint = `http://localhost:4444/`;
-export const prod_endpoint = "https://victory-select-server.herokuapp.com/";
+export const dev_endpoint = process.env.REACT_APP_API_ENDPOINT;
+export const prod_endpoint = process.env.REACT_APP_API_PRODENDPOINT;
 export const perPage = 4;
